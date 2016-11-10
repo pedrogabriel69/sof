9a0a34a9176de7623f94d9da73dc5ff7b1b717fc
Gabriel Pedro <Gabriel@Gabriels-MBP.home>
2016-11-09 20:56:10 -0500

    questions_controller and rspec for him

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ end
 
 group :test do
   gem 'shoulda-matchers'
+  gem 'rails-controller-testing'
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,128 @@
+require 'rails_helper'
+
+RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+
+  describe 'GET #index' do
+    let(:questions) { create_list(:question, 2) }
+
+    before { get :index }
+
+    it 'show all objects' do
+      expect(assigns(:questions)).to match_array(questions)
+    end
+
+    it 'render index view' do
+      expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    before { get :show, id: question }
+
+    it 'show object' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'render show view' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'new object' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'render new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    before { get :edit, id: question }
+
+    it 'edit object' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'render edit view' do
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'POST #create' do
+    context 'correct work' do
+      it 'create object' do
+        expect { post :create, question: attributes_for(:question) }.to change(Question, :count).by(1)
+      end
+
+      it 'render show view' do
+        post :create, question: attributes_for(:question)
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context 'not correct work' do
+      it 'create object' do
+        expect { post :create, question: attributes_for(:invalid_question) }.to_not change(Question, :count)
+      end
+
+      it 're-render new view' do
+        post :create, question: attributes_for(:invalid_question)
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    before { get :edit, id: question }
+
+    context 'correct work' do
+      it 'update object' do
+        patch :update, id: question, question: attributes_for(:question)
+        expect(assigns(:question)).to eq question
+      end
+
+      it 'change attributes' do
+        patch :update, id: question, question: { title: 'new title', body: 'new body' }
+        question.reload
+        expect(question.title).to eq 'new title'
+        expect(question.body).to eq 'new body'
+      end
+
+      it 'render show view' do
+        patch :update, id: question, question: { title: 'new title', body: 'new body' }
+        expect(response).to redirect_to question
+      end
+    end
+
+    context 'not correct work' do
+      it 'not change attributes' do
+        patch :update, id: question, question: { title: 'new title', body: nil }
+        question.reload
+        expect(question.title).to eq 'MyString'
+        expect(question.body).to eq 'MyText'
+      end
+
+      it 're-render edit view' do
+        patch :update, id: question, question: { title: 'new title', body: nil }
+        expect(response).to render_template :edit
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'destroy question' do
+      question
+      expect { delete :destroy, id: question }.to change(Question, :count).by(-1)
+    end
+
+    it 'redirect to index view' do
+      delete :destroy, id: question
+      expect(response).to redirect_to questions_path
+    end
+  end
+end
