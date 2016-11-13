851116be52f793b82e31f01ae4bd1af14f45e98a
Gabriel Pedro <Gabriel@Gabriels-MacBook-Pro.local>
2016-11-13 12:55:10 -0500

    modal form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   root 'questions#index'
 
-  # post '/create_question', to: 'questions#create'
+  post '/create_question', to: 'questions#create'
 
   resources :questions do
     resources :answers
