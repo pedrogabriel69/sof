df0f4010353b52b2386c8b7d5dae3ec6b2379e95
Gabriel Pedro <Gabriel@Gabriels-MBP.home>
2016-11-14 19:35:08 -0500

    fix views

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,5 +1,5 @@
 module DeviseHelper
-  def devise_error_messages!(resource)
+  def bootstrap_devise_error_messages!(resource)
     return '' if resource.errors.empty?
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
