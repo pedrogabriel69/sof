ae584fceb71207c854d341187931b65a59085587
Gabriel Pedro <Gabriel@Gabriels-MBP.home>
2016-11-24 15:07:51 -0500

    fix spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -33,10 +33,6 @@ RSpec.describe QuestionsController, type: :controller do
       expect(assigns(:answer)).to be_a_new(Answer)
     end
 
-    it 'build new attachment for answer' do
-      expect(assigns(:answer).attachments.first).to be_a_new(Attachment)
-    end
-
     it 'render show view' do
       expect(response).to render_template :show
     end
@@ -47,10 +43,6 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'new object' do
       expect(assigns(:question)).to be_a_new(Question)
-    end
-
-    it 'build new attachment for question' do
-      expect(assigns(:question).attachments.first).to be_a_new(Attachment)
     end
 
     it 'render new view' do
