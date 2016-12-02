f25206fd489bafce7d49021eeb2c555295483438
Gabriel Pedro <Gabriel@Gabriels-MBP.home>
2016-12-02 18:06:25 -0500

    spec for model vote

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Vote, type: :model do
   it { should validate_presence_of :votable }
   it { should validate_presence_of :choice }
   it { should validate_presence_of :weight }
+  it {should validate_inclusion_of(:votable_type).in_array(%w[Question Answer]) }
+
+  describe 'uniqueness validation' do
+    subject { build(:vote) }
+    it { should validate_uniqueness_of(:user_id).scoped_to(:votable_type, :votable_id) }
+  end
 end
