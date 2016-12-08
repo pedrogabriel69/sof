3a2d18786ac58df00ca343038c87b05a01303974
Gabriel Pedro <Gabriel@Gabriels-MBP.home>
2016-12-08 13:07:48 -0500

    add spec for strem answers

--- spec/features/create_answer_spec.rb
+++ spec/features/create_answer_spec.rb
@@ -5,6 +5,7 @@ feature 'User create question', '
 ' do
 
   given(:user) { create(:user) }
+  given(:guest) { create(:user) }
   given(:question) { create(:question, user: user) }
 
   scenario 'Authenticated user creates answer', js: true do
@@ -28,5 +29,39 @@ feature 'User create question', '
     click_button 'Save'
 
     expect(page).to have_content "Body can't be blank"
+  end
+
+  context "multiple sessions, answers" do
+    scenario "question appears on another user's page", js: true do
+      Capybara.using_session('user') do
+        sign_in(user)
+        visit question_path(question)
+      end
+
+      Capybara.using_session('guest') do
+        sign_in(guest)
+        visit question_path(question)
+      end
+
+      Capybara.using_session('user') do
+        fill_in('Your answer', with: 'My Answer')
+        click_button 'Save'
+
+        expect(current_path).to eq question_path(question)
+        within '.answers' do
+          expect(page).to have_content user.name
+          expect(page).to have_content 'My Answer'
+        end
+      end
+
+      Capybara.using_session('guest') do
+        visit question_path(question)
+
+        within '.answers' do
+          expect(page).to have_content user.name
+          expect(page).to have_content 'My Answer'
+        end
+      end
+    end
   end
 end
