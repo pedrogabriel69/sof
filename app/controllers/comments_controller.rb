78aba68ad6c1665d70fbee265cdc95fc1570d75e
Gabriel Pedro <Gabriel@Gabriels-MBP.home>
2016-12-08 08:59:13 -0500

    stream comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   include Commented
 
   before_action :set_commented, only: [:create]
+  after_action :publish_comment, only: [:create]
 
   def create
     @comment = @commented.comments.build(comment_params.merge(user: current_user))
@@ -12,5 +13,13 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:body)
+  end
+
+  def publish_comment
+    return if @comment.errors.any?
+    ActionCable.server.broadcast(
+      'comments',
+      render_to_string(template: 'comments/comment.json.jbuilder')
+    )
   end
 end
