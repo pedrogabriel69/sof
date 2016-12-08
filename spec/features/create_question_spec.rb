4711f0fecfed73493b1f36e4fbcc6857f3d6802e
Gabriel Pedro <Gabriel@Gabriels-MBP.home>
2016-12-08 12:03:50 -0500

    add spec for strem question

--- spec/features/create_question_spec.rb
+++ spec/features/create_question_spec.rb
@@ -5,6 +5,7 @@ feature 'User create question', '
 ' do
 
   given(:user) { create(:user) }
+  given(:guest) { create(:user) }
 
   scenario 'Authenticated user creates question' do
     sign_in(user)
@@ -22,5 +23,34 @@ feature 'User create question', '
     visit new_question_path
 
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+
+  context "multiple sessions" do
+    scenario "question appears on another user's page" do
+      Capybara.using_session('user') do
+        sign_in(user)
+        visit questions_path
+      end
+
+      Capybara.using_session('quest') do
+        sign_in(quest)
+        visit questions_path
+      end
+
+      Capybara.using_session('user') do
+        click_on('Ask question')
+        fill_in('Title', with: 'Test question')
+        fill_in('Your question', with: 'body text')
+        click_button 'Save'
+
+        expect(page).to have_content 'Test question'
+        expect(page).to have_content 'body text'
+      end
+
+      Capybara.using_session('quest') do
+        expect(page).to have_content 'Test question'
+        expect(page).to have_content 'body text'
+      end
+    end
   end
 end
