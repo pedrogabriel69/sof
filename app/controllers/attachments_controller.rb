53ebf16579042c740980fd88f5dd5473f9cb0e1f
Gabriel Pedro <Gabriel@Gabriels-MBP.home>
2016-12-09 19:31:04 -0500

    add responders to attachments controller

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,8 +1,16 @@
 class AttachmentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_attachment, only: :destroy
+
+  respond_to :js
 
   def destroy
+    respond_with(@attachment.destroy) if current_user.author?(@attachment.attachmentable)
+  end
+
+  private
+
+  def set_attachment
     @attachment = Attachment.find(params[:id])
-    @attachment.destroy if current_user.author?(@attachment.attachmentable)
   end
 end
