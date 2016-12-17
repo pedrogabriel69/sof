9f5916c061fb6769f458bee9ba1b53b49ba53bbd
Gabriel Pedro <Gabriel@Gabriels-MBP.home>
2016-12-17 14:58:49 -0500

    add glyphicon

--- app/helpers/answer_helper.rb
+++ app/helpers/answer_helper.rb
@@ -1,7 +1,7 @@
 module AnswerHelper
   def best_btn(question, answer)
-    btn_class = answer.flag ? 'btn-success' : 'btn-default'
-    link_to 'Best', best_question_answer_path(question, answer),
-            method: :put, remote: true, class: "btn #{btn_class} btn-xs"
+    btn_class = answer.flag ? '' : '-empty'
+    link_to '', best_question_answer_path(question, answer),
+            method: :put, remote: true, class: "glyphicon glyphicon-star#{btn_class}"
   end
 end
