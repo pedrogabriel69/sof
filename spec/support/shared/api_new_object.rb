0a6d214fa66c66926e5372162b0caa33811288db
Gabriel Pedro <Gabriel@Gabriels-MBP.home>
2016-12-25 17:38:24 -0500

    fix

--- spec/support/shared/api_new_object.rb
+++ spec/support/shared/api_new_object.rb
@@ -1,27 +1,27 @@
 shared_examples_for 'API create new object' do
   context 'valid params' do
-    let(:subject) { post "/api/v1/questions/#{valid}", access_token: access_token.token, format: :json, "#{instance}": attributes_for("#{instance}") }
+    let(:request) { post "/api/v1/questions/#{valid}", access_token: access_token.token, format: :json, "#{instance}": attributes_for("#{instance}") }
 
     it 'returns status 201' do
-      subject
+      request
       expect(response.status).to eq 201
     end
 
     it 'creates new question' do
-      expect { subject }.to change(association, :count).by(1)
+      expect { request }.to change(association, :count).by(1)
     end
   end
 
   context 'invalid params' do
-    let(:subject) { post "/api/v1/questions/#{valid}", access_token: access_token.token, format: :json, "#{instance}": attributes_for("#{type_instance}") }
+    let(:request) { post "/api/v1/questions/#{valid}", access_token: access_token.token, format: :json, "#{instance}": attributes_for("#{type_instance}") }
 
     it 'returns status 422' do
-      subject
+      request
       expect(response.status).to eq 422
     end
 
     it "doest't creates new answer" do
-      expect { subject }.to_not change(association, :count)
+      expect { request }.to_not change(association, :count)
     end
   end
 end
