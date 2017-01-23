1c9d7356a1746e37e69302415311b698e3e11cc4
Gabriel Pedro <Gabriel@Gabriels-MBP.home>
2017-01-22 23:16:09 -0500

    Add acceptance specs for follow

---
+++ spec/features/create_follow_spec.rb
@@ -0,0 +1,22 @@
+require_relative 'features_helper'
+
+feature 'User create subscribe to question', '
+  I want to be able subscribe to question
+' do
+
+  let(:user) { create(:user) }
+  let(:other_user) { create(:user) }
+  let(:question) { create(:question, user: other_user) }
+
+  scenario 'Authenticated user subscribe/unsubscribe to question', js: true do
+    sign_in(user)
+
+    visit question_path(question)
+
+    click_on('Subscribe')
+    expect(page).to have_link 'Unsubscribe'
+
+    click_on('Unsubscribe')
+    expect(page).to have_link 'Subscribe'
+  end
+end
