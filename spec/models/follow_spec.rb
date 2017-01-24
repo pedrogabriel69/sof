09721f2450838d3111970f781cafacd461a8af67
Gabriel Pedro <Gabriel@Gabriels-MBP.home>
2017-01-24 14:24:24 -0500

    fix spec follow model

--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -3,5 +3,9 @@ require 'rails_helper'
 RSpec.describe Follow, type: :model do
   it { should belong_to :followable }
   it { should belong_to :user }
-  it { should validate_uniqueness_of(:user_id).scoped_to(:followable_id) }
+
+  describe 'uniqueness validation' do
+    subject { build(:follow) }
+    it { should validate_uniqueness_of(:user_id).scoped_to(:followable_id) }
+  end
 end
