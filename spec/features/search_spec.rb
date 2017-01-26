3d6d01bab4610b18b6065f4d7e60b7fcda05a4cc
Gabriel Pedro <Gabriel@Gabriels-MBP.home>
2017-01-26 17:10:17 -0500

    fix

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -4,11 +4,14 @@ feature 'Search' do
   let(:user) { create(:user) }
   let!(:question) { create(:question, title: 'Title', body: 'Sphinx, RSpec, Rails', user: user) }
 
-  scenario 'User implements search', js: true do
+  before do
+    index
     visit search_path
+  end
 
+  scenario 'Success, user implements search', js: true do
     within ".search" do
-      select "Global", :from => "Choose type"
+      select "Global", from: "Choose type"
       fill_in 'Add something...', with: 'Sphinx, RSpec, Rails'
       click_on 'Search'
     end
@@ -16,11 +19,9 @@ feature 'Search' do
     expect(page).to have_content "Sphinx, RSpec, Rails"
   end
 
-  scenario 'User implements search and no matches', js: true do
-    visit search_path
-
+  scenario 'Fail, user implements search', js: true do
     within ".search" do
-      select "Global", :from => "Choose type"
+      select "Global", from: "Choose type"
       fill_in 'Add something...', with: 'JS'
       click_on 'Search'
     end
