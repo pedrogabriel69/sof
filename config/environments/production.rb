1af0bfb3cdf8a2e3d96f5137e164ad7dd8370ad3
Gabriel Pedro <Gabriel@Gabriels-MBP.home>
2017-01-29 22:57:37 -0500

    add config for mailer, production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,4 +20,6 @@ Rails.application.configure do
   end
 
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: '104.236.113.199', port: 4321 }
 end
