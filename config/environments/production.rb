6b3493ea7be432cb9bf4ef34a29175a8fcfb0bd7
Gabriel Pedro <Gabriel@Gabriels-MBP.home>
2017-02-17 11:24:07 -0500

    production config fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
 
+  config.web_socket_server_url = "ws://104.236.113.199/cable"
+  # config.action_cable.allowed_request_origins = ["http://104.236.113.199"]
+  
   if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
